95f74da29123dc668eb41b3524e77da962224e0e
Andrew Schmidthuber <aschmidthuber@gmail.com>
2016-01-18 20:07:52 -0600

    Update piglatin.js

--- piglatin.js
+++ piglatin.js
@@ -5,6 +5,14 @@ Translate the provided string to pig latin.
 Pig Latin takes the first consonant (or consonant cluster) of an English word, moves it to the end of the word and suffixes an "ay".
 
 If a word begins with a vowel you just add "way" to the end.
+
+Test Cases
+translate("california") should return "aliforniacay".
+translate("paragraphs") should return "aragraphspay".
+translate("glove") should return "oveglay".
+translate("algorithm") should return "algorithmway".
+translate("eight") should return "eightway".
+
 */
 
 
